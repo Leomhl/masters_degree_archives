8bc4fb9594bad35bfcbae5b6c3e559c219ca19d2
Leonardo Marinho <leonardomarinho@MacBook-Pro-de-Leonardo-2.local>
2021-09-29 22:44:09 -0300

    Layout da tela de recomendações, tela de sobre e solução de problemas com o grafo

--- mana/core/urls.py
+++ mana/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('logout', views.logout, name='logout')
+    path('about', views.about, name='about'),
+    path('recommendations', views.recommendations, name='recommendations')
 ]
+
--- mana/core/views.py
+++ mana/core/views.py
@@ -1,39 +1,17 @@
-# from django.contrib.auth.decorators import login_required
-# from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-# from django.contrib.auth import logout
-
-# from .models import Receita
-
 def index(request):
-    # A rede social completa vai ficar nessa parte aqui
-
-    # receitas = Receita.objects.all()
-    #
-    # dados = {
-    #     'receitas': receitas
-    # }
-
     return render(request, 'index.html')
 
-# def receita(request, receita_id):
-    # receita = get_object_or_404(Receita, pk=receita_id)
-    #
-    # dados = {
-    #     'receita': receita
-    # }
+def about(request):
+    return render(request, 'about.html')
 
-    # return render(request, 'receita.html', dados)
-
-# Acho q n rola
-# @login_required
-# def logout(request):
-#     logout(request)
-    # return HttpResponseRedirect('admin')
+def recommendations(request):
+    return render(request, 'recommendations.html')
 
 
 # Rotas para criar
+
 # Rotas admin - OK
-# Rota da rede social toda
-# Rota da tela de buscar candidato através da vaga (cadastrar ou selecionar uma vaga?)
-# Rota da tela de pessoa recomendada
+# Rota da rede social toda - ok
+# Rota de about - ok
+# Rota da tela de buscar candidato através da vaga (selecionar uma vaga e rodar) - ok
