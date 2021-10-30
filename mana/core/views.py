9a3fa3b4084c70ccdd6a88b63a084ab5998c8cef
Leonardo Marinho <leonardomarinho@MacBook-Pro-de-Leonardo-2.local>
2021-10-30 11:47:50 -0300

    Alterações nas views

--- mana/core/views.py
+++ mana/core/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from core.models import Profissional, Vaga
+from django.core import serializers
+import json
+from core.models import *
 
 def index(request):
     return render(request, 'index.html')
@@ -20,7 +22,36 @@ def professionals(request):
 
 # Core route of the application
 def recommendationsapi(request):
-    # seguir daqui
-    profissionais = list(Profissional.objects.select_related('cultura', 'maturidade_academica', 'maturidade_profissional').values())
-    print(profissionais)
-    return JsonResponse(profissionais, safe=False)
+
+    # Primeira função
+    # 1 Ver o agrupamento na rede de um candidato para um profissional setado na vaga
+    # 2 áreas de atuação do profissional e área da vaga e se são similares - ok
+    # 3 popularidade na rede, o quanto o indivíduo está conectado com ela
+
+    # Segunda função
+    # 1 Maturidade profissional do candidato bater com a da vaga - ok
+    # 2 Maturidade acadêmica do candidato bater com a da vaga - ok
+    # 3 Número de premiações - ok
+    # 4 Adaptação de cultura - ok
+
+    # profissionais = Profissional.objects.values()
+    # culturas = Cultura.objects.values()
+
+
+    # {
+    # id: 123, #id do profissional,
+    # areas_similares: 1, #quantidade de áreas que são similares a da vaga
+    # numero_premiacoes: 2, #quantidade de prêmios que tem
+    # maturidade_acadêmica: true, #se tem ou não a maturidade exigida
+    # maturidade_profissional: true, #se tem ou não a maturidade exigida
+    # adaptacao_cultura: true, #se ele se adapta ou não,
+    # agrupamento: 123, #a definir,
+    # popularidade: 123, #o quão conectado a outros nós o profissional está
+    # }
+
+
+    profissionais = Profissional.objects.all()
+    tmpJson = serializers.serialize("json", profissionais)
+    tmpObj = json.loads(tmpJson)
+
+    return JsonResponse(tmpObj, safe=False)
